a7a51b359b9a8702ce2b1edfd9ae5081fb5b0a42
guzegui <guzmanegui@gmail.com>
2024-05-09 21:23:12 +0200

    Done Iteration 4

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/home.html");
 });
 
+app.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/views/blog.html");
+});
+
+
 // START THE SERVER
 // Make your Express server listen on port 5005:
 app.listen(port, () => {
